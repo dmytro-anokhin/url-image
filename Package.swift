2c7465c24cb9459c4853a574e6dcee424c3879ec
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2019-08-21 09:12:24 +0200

    Added supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "URLImage",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
