3eab70f933669c38ea68c09855f1987bbf70d3b0
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2019-10-16 02:43:27 +0200

    Removed commented out code

--- Sources/URLImage/ImageLoader/URLSessionDownloadDelegateWrapper.swift
+++ Sources/URLImage/ImageLoader/URLSessionDownloadDelegateWrapper.swift
@@ -27,26 +27,18 @@ final class URLSessionDelegateWrapper: NSObject, URLSessionDataDelegate, URLSess
     var completeCallback: CompleteCallback?
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        // print("didFinishDownloadingTo \(downloadTask.originalRequest?.url?.lastPathComponent)")
         finishDownloadingCallback?(downloadTask, location)
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        // print("didWriteData \(downloadTask.originalRequest?.url?.lastPathComponent)")
         writeDataCallback?(downloadTask, bytesWritten, totalBytesWritten, totalBytesExpectedToWrite)
     }
 
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        // print("didCompleteWithError \(task.originalRequest?.url?.lastPathComponent) error: \(error)")
         completeCallback?(task, error)
     }
 
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        // print("didReceive data \(dataTask.originalRequest?.url?.lastPathComponent)")
         receiveDataCallback?(dataTask, data)
     }
-
-//    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-//        print("didReceive response \(dataTask.originalRequest?.url?.lastPathComponent)")
-//    }
 }
