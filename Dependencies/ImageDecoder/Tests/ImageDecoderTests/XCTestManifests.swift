87bc7d78e829e5eebad530ae2634253ec83ca3a0
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2020-09-27 18:59:03 +0200

    Use ImageDecoder as target

--- Dependencies/ImageDecoder/Tests/ImageDecoderTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(ImageDecoderTests.allTests),
-    ]
-}
-#endif
--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "Dependencies/DownloadManager"),
         .package(path: "Dependencies/FileIndex"),
-        .package(path: "Dependencies/ImageDecoder")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -32,6 +31,19 @@ let package = Package(
             dependencies: [ "RemoteContentView", "DownloadManager", "ImageDecoder", "FileIndex" ]),
         .target(
             name: "RemoteContentView"),
+        .target(
+            name: "ImageDecoder",
+            dependencies: []),
+        .testTarget(
+            name: "ImageDecoderTests",
+            dependencies: ["ImageDecoder"],
+            resources: [ .copy("Resources/TestImages.json"),
+                         .copy("Resources/lenna.jpg"),
+                         .copy("Resources/lenna.png"),
+                         .copy("Resources/sea_animation.heics"),
+                         .copy("Resources/gif-loop-count.gif"),
+                         .copy("Resources/quicksort.gif")
+            ]),
         .testTarget(
             name: "URLImageTests",
             dependencies: ["URLImage"]),
