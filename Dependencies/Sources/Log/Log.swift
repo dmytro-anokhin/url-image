b7794b75ad86f4e51100bb33a82fb457156494b9
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2020-10-16 06:14:04 +0200

    Added function argument and message is now optional

--- Dependencies/Sources/Log/Log.swift
+++ Dependencies/Sources/Log/Log.swift
@@ -8,46 +8,63 @@
 import Foundation
 import os.log
 
+public let log_default = 1
+public let log_normal = 2
+public let log_detailed = 3
+
 let log_none = Int.min
-let log_default = 1
-let log_normal = 2
-let log_detailed = 3
 let log_all = Int.max
 
-fileprivate let log_detail = log_none
+private let log_detail = log_none
 
 
 @inline(__always)
-private func log(_ object: Any? = nil, _ message: String, type osLogType: OSLogType = .default) {
-    if let object = object {
-        let typeString = String(reflecting: type(of: object))
-        os_log("[%@] %@", log: .default, type: osLogType, typeString, message)
-    }
-    else {
-        os_log("%@", log: .default, type: osLogType, message)
+private func log(_ object: Any? = nil, _ function: String?, _ message: String?, type osLogType: OSLogType = .default) {
+    switch (object, function, message) {
+
+        case (.some(let object), .some(let function), .some(let message)):
+            let typeString = String(reflecting: type(of: object))
+            os_log("[%@] (%@) %@", log: .default, type: osLogType, typeString, function, message)
+
+        case (.some(let object), .some(let function), _):
+            let typeString = String(reflecting: type(of: object))
+            os_log("[%@] (%@)", log: .default, type: osLogType, typeString, function)
+
+        case (.some(let object), _, .some(let message)):
+            let typeString = String(reflecting: type(of: object))
+            os_log("[%@] %@", log: .default, type: osLogType, typeString, message)
+
+        case (.some(let object), _, _):
+            let typeString = String(reflecting: type(of: object))
+            os_log("[%@]", log: .default, type: osLogType, typeString)
+
+        case (_, _, .some(let message)):
+            os_log("%@", log: .default, type: osLogType, message)
+
+        default:
+            break
     }
 }
 
 @inline(__always)
-func log_info(_ object: Any?, _ message: String) {
-    log(object, message, type: .info)
+public func log_info(_ object: Any?, _ function: String?, _ message: String) {
+    log(object, function, message, type: .info)
 }
 
-@inline(__always)
-func log_debug(_ object: Any?, _ message: String, detail: Int = log_default) {
+public func log_debug(_ object: Any?, _ function: String?, _ message: @autoclosure () -> String?, detail: Int = log_default) {
     guard detail < log_detail else {
         return
     }
 
-    log(object, message, type: .debug)
+    log(object, function, message(), type: .debug)
 }
 
 @inline(__always)
-func log_error(_ object: Any?, _ message: String) {
-    log(object, message, type: .error)
+public func log_error(_ object: Any?, _ function: String?, _ message: String) {
+    log(object, function, message, type: .error)
 }
 
 @inline(__always)
-func log_fault(_ object: Any?, _ message: String) {
-    log(object, message, type: .fault)
+public func log_fault(_ object: Any?, _ function: String?, _ message: String) {
+    log(object, function, message, type: .fault)
 }
