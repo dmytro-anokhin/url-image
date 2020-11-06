3bd694700a2cff9f138c94172424f73ea2b58942
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2020-11-06 07:07:28 +0100

    Add documentation

--- Sources/URLImage/URLImageService.swift
+++ Sources/URLImage/URLImageService.swift
@@ -18,15 +18,13 @@ public final class URLImageService {
 
     public static let shared = URLImageService()
 
-    let downloadManager = DownloadManager()
-
-    let diskCache = DiskCache()
-
-    let inMemoryCache = InMemoryCache()
-
-    init() {
-    }
-
+    /// The default options
+    ///
+    /// The default options are used to provide default values to properties when `URLImageOptions` is created. This allows to customize individual properties of the `URLImageOptions` object retaining default values.
+    ///
+    ///     let myOptions = URLImageOptions(identifier: "MyImage")
+    ///
+    /// In this example `myOptions` will retain default values set using this property.
     public var defaultOptions = URLImageOptions(identifier: nil,
                                                 expireAfter: 24 * 60 * 60,
                                                 cachePolicy: .returnCacheElseLoad(),
@@ -41,19 +39,35 @@ public final class URLImageService {
         inMemoryCache.cleanup()
     }
 
+    /// Remove all cached images from the disk and in-memory caches
     public func removeAllCachedImages() {
         diskCache.deleteAll()
         inMemoryCache.removeAll()
     }
 
+    /// Remove the image downloaded from the specified URL from the disk and in-memory caches
     public func removeImageWithURL(_ url: URL) {
         diskCache.delete(withIdentifier: nil, orURL: url)
         inMemoryCache.delete(withIdentifier: nil, orURL: url)
     }
 
+    /// Remove the image cached with the specified identifier from the disk and in-memory caches
     public func removeImageWithIdentifier(_ identifier: String) {
         diskCache.delete(withIdentifier: identifier, orURL: nil)
         inMemoryCache.delete(withIdentifier: identifier, orURL: nil)
+    }
+
+    // MARK: - Internal
+
+    let downloadManager = DownloadManager()
+
+    let diskCache = DiskCache()
+
+    let inMemoryCache = InMemoryCache()
+
+    // MARK: - Private
+
+    private init() {
     }
 
     private func performPreviousVersionCleanup() {
