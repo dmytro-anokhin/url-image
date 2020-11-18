cedfdb207dcba2aec1229f3e2053bb93460a4244
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2020-11-18 11:11:55 +0100

    Raise platforms requirement by 1

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "URLImage",
     platforms: [
-        .iOS(.v10),
-        .tvOS(.v10),
-        .macOS(.v10_12),
-        .watchOS(.v3)
+        .iOS(.v11),
+        .tvOS(.v11),
+        .macOS(.v10_13),
+        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
