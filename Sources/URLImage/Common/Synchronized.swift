1241e87f5aec8aa6b51e9eca254f020aca41955b
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2021-02-08 21:12:15 +0100

    Move synchronized property wrapper to a separate file

---
+++ Sources/URLImage/Common/Synchronized.swift
@@ -0,0 +1,36 @@
+//
+//  Synchronized.swift
+//  
+//
+//  Created by Dmytro Anokhin on 08/02/2021.
+//
+
+import Foundation
+
+
+/// Provides synchronization of the wrapped value using concurrent queue with barrier
+@propertyWrapper
+public final class Synchronized<Value> {
+
+    public var wrappedValue: Value {
+        get {
+            synchronizationQueue.sync {
+                value
+            }
+        }
+
+        set {
+            synchronizationQueue.async(flags: .barrier) {
+                self.value = newValue
+            }
+        }
+    }
+
+    public init(wrappedValue: Value) {
+        value = wrappedValue
+    }
+
+    private let synchronizationQueue = DispatchQueue(label: "Synchronized.synchronizationQueue.\(UUID().uuidString)", attributes: .concurrent)
+
+    private var value: Value
+}
--- Sources/URLImage/Service/URLImageService.swift
+++ Sources/URLImage/Service/URLImageService.swift
@@ -17,32 +17,6 @@ import Common
 import DownloadManager
 #endif
 
-@propertyWrapper
-public final class Synchronized<Value> {
-
-    public var wrappedValue: Value {
-        get {
-            synchronizationQueue.sync {
-                value
-            }
-        }
-
-        set {
-            synchronizationQueue.async(flags: .barrier) {
-                self.value = newValue
-            }
-        }
-    }
-
-    public init(wrappedValue: Value) {
-        value = wrappedValue
-    }
-
-    private let synchronizationQueue = DispatchQueue(label: "Synchronized.synchronizationQueue.\(UUID().uuidString)", attributes: .concurrent)
-
-    private var value: Value
-}
-
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public class URLImageService {
