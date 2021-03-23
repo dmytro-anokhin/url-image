2c7468bf2a7ca4f31f61b9c3b1fce25453d51f5d
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2021-03-23 06:02:27 +0100

    Add get image methods to in memory store

--- Sources/URLImage/Store/URLImageInMemoryStoreType.swift
+++ Sources/URLImage/Store/URLImageInMemoryStoreType.swift
@@ -6,7 +6,11 @@
 //
 
 import Foundation
+import CoreGraphics
 
+#if canImport(Model)
+import Model
+#endif
 
 /// The `URLImageInMemoryStoreType` describes an object used to store images in-memory for fast access.
 public protocol URLImageInMemoryStoreType: URLImageStoreType {
@@ -14,4 +18,19 @@ public protocol URLImageInMemoryStoreType: URLImageStoreType {
     func getImage<T>(_ keys: [URLImageStoreKey]) -> T?
 
     func store<T>(_ image: T, info: URLImageStoreInfo)
+}
+
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension URLImageInMemoryStoreType {
+
+    func getImage(_ identifier: String) -> CGImage? {
+        let transientImage: TransientImage? = getImage([ .identifier(identifier) ])
+        return transientImage?.cgImage
+    }
+
+    func getImage(_ url: URL) -> CGImage? {
+        let transientImage: TransientImage? = getImage([ .url(url) ])
+        return transientImage?.cgImage
+    }
 }
